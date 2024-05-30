93facd91d99ee6a60d5a9b6fcb95c6b023a5809f
ansarakbar <ansarmahmood298@gmail.com>
2024-05-30 16:36:10 +0500

    Create Navbar

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,8 @@
+
+const HeroSection = () => {
+  return (
+    <div className="h-auto md:h-[40rem] w-full flex flex-col items-center justify-center">HeroSection</div>
+  )
+}
+
+export default HeroSection
